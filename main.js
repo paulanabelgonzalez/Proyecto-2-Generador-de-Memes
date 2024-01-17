6a8bb12c46951b4c5f50c3f544c64187f96bd2a8
paula <gaby_pau@live.com.ar>
2024-01-17 17:03:40 -0300

    hice evento para el modo claro y oscuro, agregué imgs.

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-// evento para aparecer la solapa de imagen
+// evento para aparecer el contenedor de imagen e intercambiarla con la de texto
 
 const btnImagen = document.getElementById("btn_imagen");
 const contenedorImagen = document.getElementById("contenedor_imagen");
 
 btnImagen.addEventListener("click", () => {
     contenedorImagen.style.display = "block"
+    contenedorTexto.style.display = "none"
 })
 
-// evento para aparecer la solapa de texto
+// evento para aparecer el contenedor de texto e intercambiarla con la de imagen
 
 const btnTexto = document.getElementById("btn_texto");
 const contenedorTexto = document.getElementById("contenedor_texto");
 
 btnTexto.addEventListener("click", () => {
     contenedorTexto.style.display = "block"
+    contenedorImagen.style.display = "none"
 })
 
-// evento para cerrar solapas
+// evento para cerrar contenedores
 
 const btnCerrarImagen = document.getElementById("btn_cerrar__imagen");
 const btnCerrarTexto = document.getElementById("btn_cerrar__texto");
@@ -27,15 +29,31 @@ const btnCerrarTexto = document.getElementById("btn_cerrar__texto");
 // btnCerrarImagen.addEventListener("click", cerrarSolapa)
 // btnCerrarTexto.addEventListener("click", cerrarSolapa)
 
-//cerrar solapa imagen 
+//cerrar contenedor de imagen 
 
 btnCerrarImagen.addEventListener("click", () => {
     contenedorImagen.style.display = "none"
 });
 
-// cerrar solapa texto
+// cerrar contenedor de texto
 
 btnCerrarTexto.addEventListener("click", () => {
-    contenedorTexto
-        .style.display = "none"
+    contenedorTexto.style.display = "none"
 });
+
+// modo claro y modo oscuro
+
+const btnModo = document.getElementById("btn_modo")
+const modo = document.getElementById("modo")
+btnModo.addEventListener("click", () => {
+    document.body.classList.toggle("oscuro");
+    // cambia el texto del boton
+    if (modo.innerText == "Modo Claro") {
+        modo.innerText = "Modo Oscuro"
+    } else {
+        modo.innerText = "Modo Claro"
+    }
+})
+
+
+
